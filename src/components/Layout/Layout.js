b1bf9a90fd26add9300f9d92b32081cc3fb098c9
Clement Bourgoin <cb@nokto.net>
2017-12-25 12:52:58 +0100

    Add top margin to main content

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import css from './Layout.css';
+
 export default (props) => (
   <React.Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={css.Content}>
       {props.children}
     </main>
   </React.Fragment>
