fef7247027124c70d19ab963176b5ccd8493523e
Clement Bourgoin <cb@nokto.net>
2017-12-26 13:38:50 +0100

    Display message if there are no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import css from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 export default (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
         return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
       });
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={css.Burger}>
       <BurgerIngredient type="bread-top" />
