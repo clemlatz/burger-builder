e8051dd59ae1f3b1bb2510b350b0885e3887f8ad
Clement Bourgoin <cb@nokto.net>
2017-12-30 20:47:45 +0100

    Show backdrop when side drawer menu is open

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,12 +5,25 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import css from './Layout.css';
 
-export default (props) => (
-  <React.Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={css.Content}>
-      {props.children}
-    </main>
-  </React.Fragment>
-);
+export default class Layout extends React.Component {
+  state = {
+    showSideDrawer: true
+  }
+
+  _closeSideDrawer = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  render() {
+    return (
+        <React.Fragment>
+          <Toolbar />
+          <SideDrawer open={this.state.showSideDrawer} close={this._closeSideDrawer} />
+          <main className={css.Content}>
+            {this.props.children}
+          </main>
+        </React.Fragment>
+      );
+  }
+}
+
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,27 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 import css from './SideDrawer.css';
 
 export default (props) => {
+  const classes = [css.SideDrawer];
+  if (props.open) {
+    classes.push(css.Open);
+  }
+
   return (
-    <div className={css.SideDrawer}>
-      <div className={css.Logo}>
-        <Logo />
+    <React.Fragment>
+      <Backdrop show={props.open} clicked={props.close} />
+      <div className={classes.join(' ')}>
+        <div className={css.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </React.Fragment>
   );
 }
