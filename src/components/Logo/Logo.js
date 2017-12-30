af666f5331cfa28f075474baa7c36b428ed389da
Clement Bourgoin <cb@nokto.net>
2017-12-30 19:30:06 +0100

    Add logo to toolbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import burgerLogo from '../../assets/images/burger-logo.png';
+
+import css from './Logo.css';
+
+const logo = (props) => (
+  <div className={css.Logo}>
+    <img src={burgerLogo} alt="Clément's Burgers" />
+  </div>
+);
+
+export default logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Logo from '../../Logo/Logo';
+
 import css from './Toolbar.css';
 
 const toolbar = (props) => (
   <header className={css.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>
       ...
     </nav>
