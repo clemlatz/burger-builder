faa0bbff5bc25d8b17e88ece3c58ac1a9ce43016
Clement Bourgoin <cb@nokto.net>
2017-12-31 01:47:25 +0100

    Only rerender modal content if modal is shown

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,24 +5,32 @@ import Backdrop from '../Backdrop/Backdrop';
 
 import css from './Modal.css';
 
-const modal = (props) => (
-  <React.Fragment>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div className={css.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? 1 : 0
-        }}
-      >
-      {props.children}
-    </div>
-  </React.Fragment>
-);
+class Modal extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.show !== this.props.show;
+  }
 
-modal.propTypes = {
+  render() {
+    return (
+      <React.Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div className={css.Modal}
+            style={{
+              transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+              opacity: this.props.show ? 1 : 0
+            }}
+          >
+          {this.props.children}
+        </div>
+      </React.Fragment>
+    );
+  }
+};
+
+Modal.propTypes = {
   children: PropTypes.element.isRequired,
   modalClosed: PropTypes.func.isRequired,
   show: PropTypes.bool.isRequired
 };
 
-export default modal;
+export default Modal;
